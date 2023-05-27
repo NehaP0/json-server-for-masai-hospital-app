de0002049e3980aec4a33803953485f96397c973
NehaP0 <nehaphadtare334@gmail.com>
2023-05-27 08:45:12 +0530

    commit

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use("", router);
-server.listen(process.env.PORT || 5000, () => {
+server.use(router);
+server.listen(5000, () => {
   console.log("JSON Server is running");
 });
 
